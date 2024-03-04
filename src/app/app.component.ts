a9bd5ab6d7393ed035142cffb226ee0ffab3137f
johannesstroebele91 <stroebele.j@gmail.com>
2024-03-04 12:32:00 +0100

    Add toolbar with router-outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,48 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {MatButton} from "@angular/material/button";
+import {Component} from "@angular/core";
+import {MatNativeDateModule} from "@angular/material/core";
+import {Router, RouterLink, RouterOutlet} from "@angular/router";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatCalendar} from "@angular/material/datepicker";
+import {MatIcon} from "@angular/material/icon";
+import {MatMenu, MatMenuTrigger} from "@angular/material/menu";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {NgIf} from "@angular/common";
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButton],
+  imports: [MatToolbarModule, MatNativeDateModule, RouterOutlet, MatButton, MatCalendar, MatIcon, MatMenuTrigger, MatIconButton, MatMenu, MatCard, MatCardTitle, MatCardHeader, MatCardContent, MatToolbar, RouterLink, NgIf],
   template: `
-    <div style="margin: 12px">
-      <h1>Welcome to {{ title }}!</h1>
-      <button mat-stroked-button color="primary">Test</button>
-      <router-outlet/>
+    <mat-toolbar color="primary">
+      <mat-toolbar-row>
+        <span style="letter-spacing: 1.2px">{{ title }}<span class="material-icons"
+                                                             style="font-size: 18px; margin-left: 3px">
+directions_run
+</span></span>
+        <span style="flex: 1 1 auto;"></span>
+        <a *ngIf="isNotHomeRoute()" routerLink="/" (click)="logout()"
+           style="color: white; text-decoration: none; font-size: 16px">Logout</a>
+      </mat-toolbar-row>
+    </mat-toolbar>
+    <div style="max-width: 600px; margin: 60px auto;">
+      <router-outlet></router-outlet>
     </div>
+
   `,
-  styles: [],
 })
 export class AppComponent {
   title = 'FoxyFit';
+
+  constructor(private router: Router, private authService: AuthService) {
+  }
+
+  isNotHomeRoute(): boolean {
+    return this.router.url !== '/';
+  }
+
+  logout() {
+    this.authService.logout()
+  }
 }
